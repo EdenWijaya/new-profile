efded25e717269fd9277436c5e753b30f9ad4a77
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-02-24 16:59:34 +0700

    update about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,4 @@
-import { assets } from '@/assets/assets'
+import { assets, infoList } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -9,12 +9,24 @@ const About = () => {
         <h2 className='text-center text-5xl font-Ovo'>About Me</h2>
 
         <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-            <div>
+            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
                 <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
             </div>
 
             <div className='flex-1'>
+                <p className='mb-10 max-w-2xl font-Ovo'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac nisl nec purus scelerisque aliquet. Nulla facilisi. Nullam et nunc ut eros vehicula ultricies. Sed et
+                </p>
 
+                <ul>
+                    {infoList.map(({icon, iconDark, title, description}, index)=>(
+                        <li key={index}>
+                            <Image src={icon} alt={title}/>
+                            <h3>{title}</h3>
+                            <p>{description}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     </div>
