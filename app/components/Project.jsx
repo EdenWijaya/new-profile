7d0b881802d319278ed8fc1156d53d6d94dc30e6
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-02-27 01:57:47 +0700

    update project

--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Project = () => {
   return (
     <div id='project' className='w-full px-[-12%] py-10 scroll-mt-20'>
-        {/* <h4 className='text-center mb-2 text-lg font-Ovo'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>
             My Portfolio
-        </h4> */}
+        </h4>
         <h2 className='text-center text-5xl font-Ovo'>
             My Latest Work
         </h2>
