2009a0b96a19c1eb4bfc36352878c9dbbcc10124
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-03-13 20:32:42 +0700

    update navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -56,20 +56,20 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
                     <Image src={ isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6' />
                 </button>
 
-                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>
-                    contact <Image src={assets.arrow_icon} alt='' className='w-3'/>
+                <a href="#kontak" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>
+                    contact <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt='' className='w-3'/>
                 </a>
 
                 <button className='block md:hidden ml-3' onClick={openMenu}>
-                    <Image src={assets.menu_black} alt='' className='w-6' />
+                    <Image src={isDarkMode ? assets.menu_white : assets.menu_black} alt='' className='w-6' />
                 </button>
             </div>
 
             {/* mobile menu */}
-            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 bg-rose-50 transition duration-1000'>
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 bg-rose-50 transition duration-1000 dark:bg-darkHover dark:text-white'>
 
                 <div className='absolute right-16 top-6' onClick={closeMenu}>
-                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+                    <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer'/>
                 </div>
 
                 <li>
