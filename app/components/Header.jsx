f20206a3cefe53da7a1ff04a20859968b9f8e43e
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-03-13 23:02:40 +0700

    add motion header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,29 +2,59 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 import { motion } from 'motion/react'
+// import { y } from 'framer-motion/dist/types.d-B50aGbjN'
 
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
-        <div>
+        <motion.div
+        initial={{ scale:  0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 1, type: 'spring', stiffness: 100 }}
+        >
             <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
-        </div>
-        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
+        </motion.div>
+
+        <motion.h3 
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.5 }}
+        className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
                 Hi! I'm Eden Wijaya <Image src={assets.hand_icon} alt='' className='w-6'/>
-        </h3>
-        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+        </motion.h3>
+
+        <motion.h1 
+        initial={{ y: -30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.9, delay: 0.8 }}
+        className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
             saya meruapakan mahasiswa 
-        </h1>
-        <p className='max-w-2xl mx-auto font-Ovo '>
+        </motion.h1>
+
+        <motion.p 
+        initial={{ y: -30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 1 }}
+        className='max-w-2xl mx-auto font-Ovo '>
             saya meruapakan mahasiswa yang sedang belajar tentang web development
-        </p>
+        </motion.p>
+
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a href="#kontak" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>
+            <motion.a 
+            initial={{ y: 30, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1 }}
+            href="#kontak" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>
                 Contact <Image src={assets.right_arrow_white} alt='' className='w-4'/>
-            </a>
-            <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:text-black bg-white dark:border-white/50'>
+            </motion.a>
+
+            <motion.a 
+            initial={{ y: 30, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1.2 }}
+            href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:text-black bg-white dark:border-white/50'>
                 My Resume <Image src={assets.download_icon} alt='' className='w-4'/>
-            </a>
+            </motion.a>
         </div>
     </div>
   )
